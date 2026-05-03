045460feae6e1d19dd3d4db117cfbb75632d4fa9
bigo <basssam1993@gmail.com>
2026-05-04 00:52:39 +0300

    chore: add localhost and 127.0.0.1 ports 5176 and 5177 to allowed origins

--- convex/auth.ts
+++ convex/auth.ts
@@ -14,7 +14,11 @@ const trustedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5174",
   "http://localhost:5175",
-  "http://127.0.0.1:5175"
+  "http://127.0.0.1:5175",
+  "http://localhost:5176",
+  "http://127.0.0.1:5176",
+  "http://localhost:5177",
+  "http://127.0.0.1:5177"
 ].filter((origin): origin is string => Boolean(origin));
 const siteUrl = process.env.SITE_URL ?? "http://localhost:5173";
 
